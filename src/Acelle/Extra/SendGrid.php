7286f826406fe138b4001ef3e838dcce08963605
Louis <louisitvn@gmail.com>
2016-11-28 07:01:00 +0000

    v1.0.0

--- src/Acelle/Extra/SendGrid.php
+++ src/Acelle/Extra/SendGrid.php
@@ -4,9 +4,55 @@ namespace Acelle\Extra;
 
 class SendGrid
 {
-    public function say() 
+    const SENDGRID_BASE_URI = 'https://api.sendgrid.com/v3/';
+
+    protected $client;
+    protected $module;
+
+    public function __construct($username, $password) 
     {
-        echo "Hello world";
+        $this->client = new \GuzzleHttp\Client(['base_uri' => self::SENDGRID_BASE_URI, 'auth' => [$username, $password]]);
+    }
+
+    private function getClient()
+    {
+        return $this->client;
+    }
+
+    public function subusers()
+    {
+        $this->module = 'subusers';
+        return $this;
+    }
+
+    public function ips()
+    {
+        $this->module = 'ips';
+        return $this;
+    }
+
+    public function api_keys()
+    {
+        $this->module = 'api_keys';
+        return $this;
+    }
+
+    public function request($method, $params = [])
+    {
+        if (is_null($this->module)) {
+            throw new \Exception('method not allowed');
+        }
+        $response = $this->getClient()->request($method, $this->module, ['json' => $params ]);
+        return $response;
+    }
+
+    public function delete($subuser_name)
+    {
+        if ($this->module == 'subusers') {
+            $this->getClient()->request('DELETE', 'subusers/' . urlencode($subuser_name));
+        } else {
+            throw new \Exception('method not allowed');
+        }
     }
 }
 
